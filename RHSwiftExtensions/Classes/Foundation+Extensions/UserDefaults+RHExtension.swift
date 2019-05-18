25740ca71f383978f8dfb54ce4af2237227c811d
rongheng <495929699g@gmail.com>
2019-05-18 14:32:19 +0800

    1. v3.4版本优化中

--- RHSwiftExtensions/Classes/Foundation+Extensions/UserDefaults+RHExtension.swift
+++ RHSwiftExtensions/Classes/Foundation+Extensions/UserDefaults+RHExtension.swift
@@ -11,20 +11,19 @@ import Foundation
 
 public extension UserDefaults {
     
-    /// 保存自定义对象 类型限制 Codable
-    func setObject<T>(value : T, forKey key : String) where T : Codable  {
+    /// 保存自定义对象 已同步。
+    func setObject<T : Codable>(value : T, forKey key : String)  {
         let data = try? JSONEncoder().encode(value)
         set(data, forKey: key)
         synchronize()
     }
     
     /// 获取自定义对象
-    func getObject<T>(forKey key : String) -> T? where T : Codable {
+    func getObject<T : Codable>(forKey key : String) throws -> T {
         guard let data = object(forKey: key) as? Data
-            else { return nil }
+            else { throw NSError() }
         
-        let value = try? JSONDecoder().decode(T.self, from: data)
-        return value
+        return try JSONDecoder().decode(T.self, from: data)
     }
     
 }
